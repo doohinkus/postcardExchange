0d262a795950642b32d6a86c5aa9edca9571290d
Rafael P <doohinkus@gmail.com>
2017-03-06 10:35:15 -0800

    add pair function.

--- src/app/pair/pair.component.ts
+++ src/app/pair/pair.component.ts
@@ -16,15 +16,23 @@ export class PairComponent implements OnInit {
 
   connectUsers(){
     var count: number = 0;
+    var keys = [];
     this.users = this.UserServiceService.getUsers();
     this.users.forEach((data) => {
-      count++;
+   //grab keys
       data.forEach((entry) => {
-        this.UserServiceService.pairUsers(entry.$key, "test");
-
-
-      })
+        keys.push(entry.$key);
+      });
+      keys.forEach((key, index) => {
+        if (index < keys.length-1){
+          this.UserServiceService.pairUsers(keys[index], keys[index+1]);
+        }else{
+          this.UserServiceService.pairUsers(keys[index], keys[0]);
+        }
+        // console.log(key, index);
+      });
     });
+    // this.UserServiceService.pairUsers(entry.$key, "test");
   }
 
  constructor(private UserServiceService: UserServiceService) { }
