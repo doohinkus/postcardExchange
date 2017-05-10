8cb7f54b55f7ccaa705de706eb651e47a2e5df71
doohinkus <doohinkus@gmail.com>
2017-05-10 10:22:07 -0700

    add new branch

--- src/app/api-keys.ts
+++ src/app/api-keys.ts
@@ -1,8 +1,14 @@
 export var masterFirebaseConfig = {
   //sk8app is the database
-  apiKey: "AIzaSyCFzGbf9egg7Vo68BKDmpqbWm0PlceqjEY",
-  authDomain: "sk8app-785f7.firebaseapp.com",
-  databaseURL: "https://sk8app-785f7.firebaseio.com",
-  storageBucket: "sk8app-785f7.appspot.com",
-  messagingSenderId: "260304210972"
+  // apiKey: "AIzaSyCFzGbf9egg7Vo68BKDmpqbWm0PlceqjEY",
+  // authDomain: "sk8app-785f7.firebaseapp.com",
+  // databaseURL: "https://sk8app-785f7.firebaseio.com",
+  // storageBucket: "sk8app-785f7.appspot.com",
+  // messagingSenderId: "260304210972"
+   apiKey: "AIzaSyA3d6KW21tdhQWAnHZZi3NgvEWUxNKDc-Q",
+   authDomain: "postcardexchange-1488491588529.firebaseapp.com",
+   databaseURL: "https://postcardexchange-1488491588529.firebaseio.com",
+   projectId: "postcardexchange-1488491588529",
+   storageBucket: "postcardexchange-1488491588529.appspot.com",
+   messagingSenderId: "856171497787"
   };
