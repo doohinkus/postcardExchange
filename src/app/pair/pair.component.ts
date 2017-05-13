93998f3dec7c205ec41cf881c57172330a7f8a0f
doohinkus <doohinkus@gmail.com>
2017-05-13 15:47:39 -0700

    add randomize feature to pairings

--- src/app/pair/pair.component.ts
+++ src/app/pair/pair.component.ts
@@ -23,14 +23,21 @@ export class PairComponent implements OnInit {
       data.forEach((entry) => {
         keys.push(entry.$key);
       });
-      keys.forEach((key, index) => {
-        if (index < keys.length-1){
-          this.UserServiceService.pairUsers(keys[index], keys[index+1]);
-        }else{
-          this.UserServiceService.pairUsers(keys[index], keys[index-index]);
-        }
-        console.log(key, index);
-      });
+    });
+   //shuffle array
+    keys.sort(function(){
+      //toggles between 0 and 1, moving elements forward or backward
+      //not truly random, but it will serve
+      return .5 - Math.random();
+    })
+    //pair users with person below them in the array, last person links with top person
+    keys.forEach((key, index) => {
+      if (index < keys.length-1){
+        this.UserServiceService.pairUsers(keys[index], keys[index+1]);
+      }else{
+        this.UserServiceService.pairUsers(keys[index], keys[index-index]);
+      }
+      console.log(key, index);
     });
     // this.UserServiceService.pairUsers(entry.$key, "test");
   }
